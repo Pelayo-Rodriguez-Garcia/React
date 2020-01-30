29350908efbe3126682a944bbc7267a875d9d364
pelayo-rodriguez-garcia <pelayo.r.g@gmail.com>
2020-01-30 20:05:53 +0100

    Mutabilidad, Funciones Componente y Turnos
    Ventajas de la inmutabilidad (ayuda a React a saber cuándo un objeto cambia). Una clase componente sin estado
    puede ser implementada sencilllamente como una función que recibe props. Implementamos turnos usando
    una variable booleana y el operador ternario.

--- src/index.js
+++ src/index.js
@@ -3,20 +3,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-class Square extends React.Component {
 
-
-
-    render() {
-        return (
-            <button
-                className="square"
-                onClick={() => this.props.onClick()}
-            >
-                {this.props.value}
-            </button>
-        );
-    }
+function Square(props) {
+    return (
+        <button className="square" onClick={props.onClick}>
+            {props.value}
+        </button>
+    )
 }
 
 class Board extends React.Component {
@@ -24,6 +17,7 @@ class Board extends React.Component {
         super(props);
         this.state = {
             squares: Array(9).fill(null),
+            xIsNext: true,
         };
 
     }
@@ -36,12 +30,15 @@ class Board extends React.Component {
 
     handleClick(i) {
         const squares = this.state.squares.slice();
-        squares[i] = 'X';
-        this.setState({ squares: squares });
+        squares[i] = this.state.xIsNext ? 'X' : 'O'
+        this.setState({
+            squares: squares,
+            xIsNext: !this.state.xIsNext,
+        });
     }
 
     render() {
-        const status = 'Next player: X';
+        const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
 
         return (
             <div>
